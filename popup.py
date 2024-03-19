24968c791d1e9aa1f00378e4ae41686a293ffe37
Raquelle M. Campos <raquellecampos12@gmail.com>
2024-03-18 23:04:33 -0400

    pupup

--- popup.py
+++ popup.py
@@ -11,9 +11,20 @@ def main(page: ft.Page):
         on_dismiss= lambda _: print('Fechou'),
 
         actions=[
-            ft.TextButton(text='Cancelar', style=ft.ButtonStyle(color=ft.colors.RED))
-        ]
+            ft.TextButton(text='Cancelar', style=ft.ButtonStyle(color=ft.colors.RED)),
+            ft.ElevatedButton(text='Salvar', style=ft.ButtonStyle(bgcolor=ft.colors.GREEN, color=ft.colors.WHITE))
+        ],
+      actions_alignment=ft.MainAxisAlignment.END,
     )
+
+    def abrir_dialogo(e):
+        page.dialog = alerta
+        alerta.open = True
+        page.update()
+
+    btn1 = ft.ElevatedButton(text='Abrir', on_click=abrir_dialogo)
+
+    page.add(btn1)
 
     page.dialog = alerta
     alerta.open = True
